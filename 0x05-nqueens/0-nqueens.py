dcd2f5460bfe0763aeb7c3ac3d0f30dca0ff5785
sabrallah youssef <elfahssimounir7@gmail.com>
2024-05-28 23:52:32 +0100

    update

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -8,68 +8,58 @@ N×N chessboard.
 
 import sys
 
+def is_safe(board, row, col):
+    """Check if it's safe to place a queen at board[row][col]."""
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
 
-def solve_n_queens(n):
-    """
-    Solve the N Queens problem using backtracking.
-
-    Args:
-        n (int): The number of queens and the size of the chessboard.
-
-    Returns:
-        list: A list of all possible solutions. Each solution is represented
-              as a list of integers where the value of i-th integer represents
-              the column number of the queen placed in the i-th row.
-    """
-    def can_place(pos, ocuppied_positions):
-        for i in range(len(ocuppied_positions)):
-            if ocuppied_positions[i] == pos or \
-                ocuppied_positions[i] - i == pos - len(ocuppied_positions) or \
-                    ocuppied_positions[i] + i == pos + len(ocuppied_positions):
-                return False
+def solve_nqueens_util(board, col):
+    """Utilize backtracking to find all solutions."""
+    if col >= len(board):
+        print_solution(board)
         return True
+    res = False
+    for i in range(len(board)):
+        if is_safe(board, i, col):
+            board[i][col] = 1
+            res = solve_nqueens_util(board, col + 1) or res
+            board[i][col] = 0
+    return res
 
-    def place_queens(n, index, ocuppied_positions, all_ocuppied_positions):
-        if index == n:
-            all_ocuppied_positions.append(ocuppied_positions[:])
-            return
+def print_solution(board):
+    """Print the board configuration."""
+    solution = []
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 1:
+                solution.append([i, j])
+    print(solution)
 
-        for i in range(n):
-            if can_place(i, ocuppied_positions):
-                ocuppied_positions.append(i)
-                place_queens(n, index + 1, ocuppied_positions,
-                             all_ocuppied_positions)
-                ocuppied_positions.pop()
+def solve_nqueens(n):
+    """Solve the N Queens problem."""
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    if not solve_nqueens_util(board, 0):
+        print("No solution exists")
+        return
 
-    all_ocuppied_positions = []
-    place_queens(n, 0, [], all_ocuppied_positions)
-    return all_ocuppied_positions
-
-
-def main():
-    """
-    The main function of the program. It checks the command line arguments and
-    calls the function to solve the N Queens problem.
-    """
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
     try:
         n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-
-    solutions = solve_n_queens(n)
-    for solution in solutions:
-        result = [[i, pos] for i, pos in enumerate(solution)]
-        print(result)
-
-
-if __name__ == "__main__":
-    main()
+    solve_nqueens(n)
